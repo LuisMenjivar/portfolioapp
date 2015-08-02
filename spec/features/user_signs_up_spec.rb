9e4aeb736ba03f6fbd4ecf9d03c9c2dc781a6fcc
luismenjivar <luismenjivr@gmail.com>
2015-08-02 06:06:40 -0400

    added user signs up spec

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+feature "User signs up for acount" do 
+  scenario "successfuly" do 
+    visit new_user_registration_path
+    fill_in 'Email', with: "test@example.com"
+    fill_in 'Password', with: "helloworld"
+    fill_in 'Password confirmation', with: "helloworld"
+    click_button 'Sign up'
+    expect(page).to have_text("You successfuly signed up")
+    expect(unread_emails_for(User.first.email).count).to eq(1)
+    open_email(User.first.email)
+    expect(current_email).to have_body_text("You can confirm your account email through the link below")
+    click_first_link_in_email
+    expect(page).to have_content('Your email address has been successfully confirmed')
+  end  
+end
