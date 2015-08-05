bcb2899214c2a1124aa39152d3fd802b958de992
luismenjivar <luismenjivr@gmail.com>
2015-08-04 23:47:16 -0400

    added todos feature and user creates todos spec

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,30 @@
+class TodosController < ApplicationController
+
+  def index
+    authenticate_user!
+    @todos = current_user.todos.all
+    @todo = Todo.new
+  end
+
+  
+  def create
+    @todo = current_user.todos.create(todo_params)
+    if @todo.persisted?
+      redirect_to todos_path, notice:'Item was saved' 
+    else 
+      render :action => :index
+    end
+  end
+
+  def destroy 
+    @todo = Todo.where(user_id: current_user.id).find(params[:id])
+    @todo.destroy
+    redirect_to todos_path, notice: 'Todo was deleted'
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description, :id)
+  end
+end
