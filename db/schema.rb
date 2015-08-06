7e2cdcd37d28c3aadd5995cb4af073e72046521e
luismenjivar <luismenjivr@gmail.com>
2015-08-06 16:07:42 -0400

    added default value for wiky public

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806200005) do
+ActiveRecord::Schema.define(version: 20150806200536) do
 
   create_table "collaborations", force: :cascade do |t|
     t.integer  "wiky_id"
