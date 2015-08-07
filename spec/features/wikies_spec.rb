4ffce7c45d908f3a1f97b9f9b9b07826787cd142
luismenjivar <luismenjivr@gmail.com>
2015-08-07 00:25:41 -0400

    refactored test

--- spec/features/wikies_spec.rb
+++ spec/features/wikies_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 feature 'Standard user' do
   before do
-    @wiki = create(:wiky)
     @standard_user = create(:user)
     sign_in(@standard_user.email, @standard_user.password)
     @wiky = create(:wiky, user: @standard_user, title: "Valid Title")
@@ -22,16 +21,23 @@ feature 'Standard user' do
   scenario "standard user can see wikies he/she can collaborate on" do 
     visit('/privatewikies')
     expect(page).to have_content(@wiky.reload.title)
-    puts current_path
   end
 
   scenario "standard user visits Wiky show" do 
-    visit('/wiky/#{@wiky.id}')
-    expect(page).to have_text(@wiky.user)
-    expect(page).to have_text(@wiky.collaboration.user)
+    visit wiky_path(@wiky)
+    expect(page).to have_text(@wiky.user.email)
+    @wiky.collaborators do |user|  
+      expect(page).to have_text(user.email)
+    end
   end
   
   scenario "standard user edits a wiky" do 
-    visit('/')
+    visit wiky_path(@wiky)
+    click_link "edit"
+    expect(current_path).to eq(edit_wiky_path(@wiky))
+    fill_in "wiky_title", with: "this is the new title"
+    click_button "Update Wiky"
+    expect(current_path).to eq(wiky_path(@wiky))
+    expect(page).to have_text("this is the new title")
   end
 end
