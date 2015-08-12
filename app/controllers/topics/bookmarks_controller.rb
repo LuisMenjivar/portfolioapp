a976a28be80b6fac5e8054195e2ba16b056ab0f7
luismenjivar <luismenjivr@gmail.com>
2015-08-12 15:38:02 -0400

    modified topic show and bookmark index

--- app/controllers/topics/bookmarks_controller.rb
+++ app/controllers/topics/bookmarks_controller.rb
@@ -1,6 +1,7 @@
 class Topics::BookmarksController < ApplicationController
   layout 'topic_layout'
   def show
-    @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
   end
 end
