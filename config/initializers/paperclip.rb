f09339e3db9f44817af36fdd44b1284831ccceb8
luismenjivar <luismenjivr@gmail.com>
2015-08-11 20:41:20 -0400

    added paperclip config file

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+  require 'aws-sdk-v1'
+  require 'aws-sdk'
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
