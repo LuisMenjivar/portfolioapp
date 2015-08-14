efa02ee34dc6044b888351426610988aec4e1d42
luismenjivar <luismenjivr@gmail.com>
2015-08-14 16:56:10 -0400

    added quantity column to line_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814160822) do
+ActiveRecord::Schema.define(version: 20150814205437) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
@@ -45,8 +45,9 @@ ActiveRecord::Schema.define(version: 20150814160822) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
